9b34e9d8fd4ff4244e2c3dc066510854b322d0d2
LeKoArts <lekoarts@gmail.com>
2018-02-04 12:11:48 +0100

    Anpassungen

--- src/utils/typography.jsx
+++ src/utils/typography.jsx
@@ -1,36 +1,31 @@
-import Typography from "typography"
+import Typography from 'typography';
 
 const typography = new Typography({
-    title: 'LekoArts',
-    baseFontSize: `16px`,
-    baseLineHeight: 1.5,
-    headerFontFamily: ['Montserrat', 'sans-serif'],
-    bodyFontFamily: ['Istok Web', 'sans-serif'],
-    scaleRatio: 2.5,
-    headerWeight: 700,
-    googleFonts: [
-        {
-            name: 'Montserrat',
-            styles: [
-                '700',
-                '400'
-            ]
-        },
-        {
-            name: 'Istok Web',
-            styles: [
-                '400'
-            ]
-        }
-    ],
-    overrideStyles: ({ adjustFontSizeTo, rhythm }, options, styles) => ({
-        img: {
-            marginBottom: 0
-        },
-        p: {
-            marginBottom: "2rem"
-        }
-    })
-})
+  title: 'LekoArts',
+  baseFontSize: '16px',
+  baseLineHeight: 1.5,
+  headerFontFamily: ['Montserrat', 'sans-serif'],
+  bodyFontFamily: ['Istok Web', 'sans-serif'],
+  scaleRatio: 2.5,
+  headerWeight: 700,
+  googleFonts: [
+    {
+      name: 'Montserrat',
+      styles: ['700', '400'],
+    },
+    {
+      name: 'Istok Web',
+      styles: ['400'],
+    },
+  ],
+  overrideStyles: () => ({
+    img: {
+      marginBottom: 0,
+    },
+    p: {
+      marginBottom: '2rem',
+    },
+  }),
+});
 
-export default typography
+export default typography;
