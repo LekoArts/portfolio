f0a5a94338af08942ab7567636d02044de2c7fe7
LeKoArts <lekoarts@gmail.com>
2018-04-04 16:08:42 +0200

    configs

--- config/website.js
+++ config/website.js
@@ -1,18 +1,23 @@
 module.exports = {
+  // Basics
   blogPostDir: 'blog',
   projectPostDir: 'projekt',
+  // Meta Daten
   siteTitle: 'LekoArts - Kommunikationsdesigner & Front-End Entwickler',
   siteTitleAlt: 'LekoArts - Kommunikationsdesigner & Front-End Entwickler',
   siteShortName: 'LekoArts',
-  siteLogo: '/logos/logo-1024.png',
+  siteLogo: '/social/avatar.png',
   siteBanner: '/social/banner.jpg',
   siteUrl: 'https://www.lekoarts.de',
   pathPrefix: '/',
   siteDescription:
     'Hi! Ich heiße Lennart und bin sowohl autodidaktischer als auch leidenschaftlicher Kommunikationsdesigner & Front-End Entwickler. Ich entwerfe, gestalte und entwickle plattformübergreifende Design-Konzepte, um das volle Potential aus deiner Marke herauszuholen.',
+  // IDs
   googleAnalyticsID: 'UA-47519312-1',
-  userTwitter: 'lekoarts_de',
+  twitter: 'lekoarts_de',
+  facebook: 'lekoarts.de',
   disqusShortname: 'lekoarts',
+  // Manifest
   themeColor: '#3498DB',
-  backgroundColor: '#2b2e3c',
+  backgroundColor: '#3498DB',
 };
--- src/layouts/index.jsx
+++ src/layouts/index.jsx
@@ -1,50 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Helmet from 'react-helmet';
-import SEO from '../components/SEO/SEO';
+import Head from '../components/Head';
 import Navigation from '../components/Navigation/Navigation';
-import config from '../../data/SiteConfig';
-import '../utils/reboot.scss';
-import '../utils/_style.scss';
+import '../fonts/inter-ui.css';
 
-export default class MainLayout extends React.Component {
-  componentDidMount() {
-    const html = document.documentElement;
-    function changeTheme() {
-      const date = new Date();
-      const hr = date.getHours();
-      const morning = 7;
-      const evening = 20;
-      if (hr > morning && hr < evening) {
-        html.classList.remove('night');
-      }
-      if (hr >= evening) {
-        html.classList.add('night');
-      }
-      if (hr >= 0 && hr <= morning) {
-        html.classList.add('night');
-      }
-    }
-    changeTheme();
-  }
-  render() {
-    const { children } = this.props;
-    return (
-      <div className="layout">
-        <Helmet>
-          <html lang="de" />
-          <title>{config.siteTitle}</title>
-          <meta charSet="utf-8" />
-          <meta httpEquiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
-        </Helmet>
-        <SEO />
-        <Navigation />
-        {children()}
-      </div>
-    );
-  }
-}
+const MainLayout = props => {
+  const { children } = props;
+  return (
+    <div className="layout">
+      <Head />
+      <Navigation />
+      {children()}
+    </div>
+  );
+};
+
+export default MainLayout;
 
 MainLayout.propTypes = {
   children: PropTypes.func.isRequired,
