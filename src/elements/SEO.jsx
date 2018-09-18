4702da7b904b65632712ea6e4b20ddf6ddc11271
Lennart <lekoarts@gmail.com>
2018-09-18 19:47:05 +0200

    Modify SEO

--- src/elements/SEO.jsx
+++ src/elements/SEO.jsx
@@ -77,8 +77,6 @@ const SEO = props => {
       <title>{title}</title>
       <meta name="description" content={description} />
       <meta name="image" content={image} />
-      <meta charSet="utf-8" />
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
       <link rel="apple-touch-icon" href="/favicons/apple-touch-icon.png" />
       <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
       <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" />
