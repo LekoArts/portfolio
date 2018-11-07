faaf5078b1f8617d8aa5191cadc0beb0a57161e6
Lennart <lekoarts@gmail.com>
2018-11-07 16:11:21 +0100

    Absolute import components & icons

---
+++ src/icons/index.js
@@ -0,0 +1,8 @@
+import Behance from './Behance'
+import GitHub from './GitHub'
+import Instagram from './Instagram'
+import Logo from './Logo'
+import Paperplane from './Paperplane'
+import YouTube from './YouTube'
+
+export { Behance, GitHub, Instagram, Logo, Paperplane, YouTube }
