62ecfbd26963c3b463ceba0d7671fc452d8c6bcc
Lennart <lekoarts@gmail.com>
2019-01-05 20:29:12 +0100

    Delete unused file

--- src/utilities/cfl.js
+++
@@ -1,9 +0,0 @@
-/**
- * Capitalize the first letter in a string
- * @param {string} string - The text you want to be capitalized
- * @returns {string}
- */
-
-const cfl = string => string.charAt(0).toUpperCase() + string.slice(1)
-
-module.exports = cfl
