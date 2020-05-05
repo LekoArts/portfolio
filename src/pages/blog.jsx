6322be69786099c3613eadcf1329fe357087ba1c
LekoArts <lekoarts@gmail.com>
2020-05-05 15:03:22 +0200

    change imports

--- src/pages/blog.jsx
+++ src/pages/blog.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { graphql } from 'gatsby'
-import Helmet from 'react-helmet'
+import { Helmet } from 'react-helmet'
 import styled from 'styled-components'
 import { Container, Layout, SkipNavContent, FadeIn } from 'elements'
 import { ItemBlog, Footer, Header } from 'components'
--- src/pages/imprint.jsx
+++ src/pages/imprint.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Helmet from 'react-helmet'
+import { Helmet } from 'react-helmet'
 import { graphql } from 'gatsby'
 import PropTypes from 'prop-types'
 import { Container, Layout } from 'elements'
